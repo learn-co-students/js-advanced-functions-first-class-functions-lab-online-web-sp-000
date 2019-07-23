152d46a1e3022a39b4409fd81080d47dbf45f9e6
blainelawson <blaine.lawson@gmail.com>
2019-07-23 16:23:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const returnFirstTwoDrivers = function (drivers) {
     return [drivers[0], drivers[1]];
+    // return drivers.slice(0,2)
 }
 
 const returnLastTwoDrivers = function (drivers) {
     return [drivers[drivers.length-2], drivers[drivers.length-1]]
+    // return drivers.slice(-2)
 }
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
@@ -14,12 +16,9 @@ function createFareMultiplier(number) {
     }
 }
 
-const fareDoubler = function (createFareMultiplier) {
-    return createFareMultiplier * 2;
-}
+const fareDoubler = createFareMultiplier(2)
 
-const fareTripler = function(createFareMultiplier) {
-    return createFareMultiplier * 3;
+const fareTripler = createFareMultiplier(3)
 }
 
 function selectDifferentDrivers(drivers, selectingDrivers) {
