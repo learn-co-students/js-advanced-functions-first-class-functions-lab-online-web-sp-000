99f27eca4805b8dac28397042dad5cbccb15fe1a
Omar Gonzalez <ogonzaleznyc@gmail.com>
2019-10-03 20:21:13 +0000

    Done.

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ const returnFirstTwoDrivers = function (array) {
     return array.slice(0, 2)
 }
 
+
 const returnLastTwoDrivers = function(driversArray) {
-  return driversArray.slice(-2)
+  return driversArray.slice(-2);
 }
 
 // const returnLastTwoDrivers = function (array) {
 //   return array.slice(array.length -2);
 // }
 
+
 let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
 
 let createFareMultiplier = function (integer) {
   return function (fare) {
@@ -18,12 +21,15 @@ let createFareMultiplier = function (integer) {
   }
 }
 
+
 const fareDoubler = createFareMultiplier(2);
+
 
 const fareTripler = createFareMultiplier(3);
 
-function fetchSpecifiedDrivers(driversArray, returnFunction) {
-  return returnFunction(driversArray)
+
+function fetchSpecifiedDrivers(arrayOfDrivers, specifiedFunction) {
+  return specifiedFunction(arrayOfDrivers);
 }
 
 // let fetchSpecifiedDrivers = function(arrayOfDrivers, specifiedFunction) {
