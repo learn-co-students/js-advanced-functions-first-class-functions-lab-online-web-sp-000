2653c27dc2a01f1a3aa0f60d712247f4141b1e12
Andrew Davisson <adavisson@Andrews-MacBook-Air.local>
2019-10-06 12:25:27 -0600

    Done.

--- index.js
+++ index.js
@@ -1,39 +1,36 @@
 // Code your solution in this file!
-function returnFirstTwoDrivers(drivers){
-  const firstTwo = function (drivers){
-    return drivers.slice(0,2);
+function returnFirstTwoDrivers(drivers) {
+  const firstTwo = function(drivers) {
+    return drivers.slice(0, 2);
   };
   return firstTwo(drivers);
 }
 
-function returnLastTwoDrivers(drivers){
-  const lastTwo = function (drivers){
+function returnLastTwoDrivers(drivers) {
+  const lastTwo = function(drivers) {
     return drivers.slice(-2);
   };
   return lastTwo(drivers);
 }
 
-const selectingDrivers = [
-  returnFirstTwoDrivers,
-  returnLastTwoDrivers
-];
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
-function createFareMultiplier(number){
-  return function(fare){
+function createFareMultiplier(number) {
+  return function(fare) {
     return fare * number;
   };
 }
 
-function fareDoubler(fare){
+function fareDoubler(fare) {
   const doubleFare = createFareMultiplier(2);
   return doubleFare(fare);
 }
 
-function fareTripler(fare){
+function fareTripler(fare) {
   const tripleFare = createFareMultiplier(3);
   return tripleFare(fare);
 }
 
-function selectDifferentDrivers(drivers, fn){
+function selectDifferentDrivers(drivers, fn) {
   return fn(drivers);
 }
