4ea388e4083125fbd28478d17ffccb54ccc1cf1f
Robert Tate <roberttate@Roberts-MacBook-Pro.local>
2019-10-17 10:23:15 -0500

    Done.

--- index.js
+++ index.js
@@ -24,3 +24,6 @@ const fareTripler = function (fare) {
 function fetchSpecifiedDrivers(drivers, selectedFunction) {
   return selectedFunction(drivers);
 }
+
+// Completed lab as part of Version 7.  I either worked on my copy of the master or the
+// "Fork This Lab" element isn't working.
