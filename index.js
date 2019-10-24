9bcc81978f311fbd92f080b0e7168f936b97c28e
Tsunamins <reillyamr@gmail.com>
2019-10-24 13:55:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
 // Code your solution in this file!
 
+//goal: declare const variable assigned to anonymous function
+//anonymous function take array of drivers
+//and return the first 2 in the array
 const returnFirstTwoDrivers = function(drivers) {
     return drivers.slice(0,2);
 }
 
+
+//goal: declare const assign to anonymous func
+//anon func once again to take array of drivers
+//and return the last 2 in array
 const returnLastTwoDrivers = function(drivers) {
     return drivers.slice(-2);
 }
 
+
+//create a const that selects the first and last 2 drivers found in above functions
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
+//high order func
+//take arg as integer
+//returns with anon func using mulitplier times the fare
 function createFareMultiplier(theMultiplier){
     return function(fare){
         return fare * theMultiplier;
     }
 }
 
+
 const fareDoubler = createFareMultiplier(2);
+
+
 const fareTripler = createFareMultiplier(3);
+
+//
 const selectDifferentDrivers = function(drivers, func) {
     return func(drivers);
 } 
