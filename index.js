94c9d7bc236c0d6e3e9e61eeaacb914b8431b7ac
stabassum <sumu1993@gmail.com>
2020-01-04 21:42:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+
+function returnFirstTwoDrivers()
+{
+    const anonFunction = secondFunc;
+
+    function secondFunc(arrayDriver){
+        return arrayDriver[0];
+        return arrayDriver[1];
+
+    }
+}
