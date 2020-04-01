10ec1ecfaaa4ed459fcc42a7f3f7162d423954a6
Ronald Stephens <rstephens@bluemorpho.cc>
2020-04-01 18:57:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (function(drivers) {
+  const array = [];
+  array.push(drivers[0]);
+  array.push(drivers[1]);
+  return array;
+})
+
+const returnLastTwoDrivers = (function(drivers) {
+  const array = [];
+  array.push(drivers[drivers.length - 2])
+  array.push(drivers[drivers.length - 1])
+  return array;
+})
+
+const selectingDrivers = []
+  selectingDrivers.push(returnFirstTwoDrivers);
+  selectingDrivers.push(returnLastTwoDrivers);
+
+function createFareMultiplier(multiplier) {
+  let fareFunction = function(fare) {
+    return( fare * multiplier)
+  }
+  return fareFunction
+}
+
+function fareDoubler(fare) {
+  return(fare * 2)
+}
+
+function fareTripler(fare) {
+  return fare + fareDoubler(fare)
+}
+
+function selectDifferentDrivers(arrayOfDrivers, fn) {
+
+  if(fn === returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(arrayOfDrivers)
+  } else {
+    return returnLastTwoDrivers(arrayOfDrivers)
+  }
+
+}
