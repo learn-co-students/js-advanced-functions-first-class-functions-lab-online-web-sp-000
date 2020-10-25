b0c6918808d8c20f91e86f7e730430ea64f56730
chienleow <chienleow1@gmail.com>
2020-10-24 22:33:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-const returnFirstTwoDrivers = function(array) {
-    return array.slice(0,2);
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0,2);
 };
 
-const returnLastTwoDrivers = function(array) {
-   return array.slice(-2);
+const returnLastTwoDrivers = function(drivers) {
+   return drivers.slice(-2);
 };
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
-const createFareMultiplier = (integer) => (a, b) => {
-    return integer(a, b);
+const createFareMultiplier = function(integer) {
+    return function(fare) {
+        return fare * integer;
+    };
 };
 
+// shorter version 
+// const fareDoubler = createFareMultiplier(2);
+const fareDoubler = function(createFareMultiplier) {
+    return createFareMultiplier * 2;
+};
+
+const fareTripler = function(createFareMultiplier) {
+    return createFareMultiplier * 3;
+};
+
+const selectDifferentDrivers = function(drivers, whichDrivers) {
+    return whichDrivers(drivers);
+};
