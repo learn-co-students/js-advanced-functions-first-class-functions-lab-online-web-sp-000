b45635d6decdc0ea18dc7332783bfe0002455329
felipeldn91 <felipeldn91@gmail.com>
2020-11-14 15:31:30 +0000

    Done.

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ const fareDoubler = createFareMultiplier(2);
 
 const fareTripler = createFareMultiplier(3);
 
-const selectDifferentDrivers = function (drivers, whichDrivers) {
-    return whichDrivers(drivers);
+const selectDifferentDrivers = function (drivers, sliceDrivers) {
+    return sliceDrivers(drivers);
 };
