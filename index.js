24afbfd03105e3fe41d2b6e2cfc0db12963d78cb
Winstonpemberton <winstonpemberton@gmail.com>
2021-01-02 11:39:29 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,27 @@
 const returnFirstTwoDrivers = function(array){
     return array.slice(0,2)
 }
+
+const returnLastTwoDrivers = function(array){
+    return array.slice(Math.max(array.length - 2, 0))
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers ]
+
+const createFareMultiplier = function(multiplier){
+    return function(fare){
+      return fare * multiplier
+    }
+}
+
+const fareDoubler = function(fare){
+    return createFareMultiplier(2)(fare)
+  }
+
+const fareTripler = function(fare){
+    return createFareMultiplier(3)(fare)
+}
+
+const selectDifferentDrivers = function(arrayOfDrivers, aFunction){
+    return aFunction(arrayOfDrivers)
+}
