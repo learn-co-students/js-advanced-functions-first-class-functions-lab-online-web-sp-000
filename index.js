bf29eaa92a8fd4453e1c868b015e1f9b677345a1
ArslanInfinite <ali.arslan319@gmail.com>
2021-01-29 17:06:12 -0500

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Code your solution in this file!
 
 const returnFirstTwoDrivers = function (drivers) {
-  firstTwoDrivers = drivers.slice(0, 2)
+  const firstTwoDrivers = drivers.slice(0, 2)
   return firstTwoDrivers
 }
 
 const returnLastTwoDrivers = function (drivers) {
-  lastTwoDrivers = drivers.slice(-2)
+  const lastTwoDrivers = drivers.slice(-2)
   return lastTwoDrivers
 }
 
@@ -22,6 +22,6 @@ const fareDoubler = createFareMultiplier(2)
 
 const fareTripler = createFareMultiplier(3)
 
-const selectDifferentDrivers = function(drivers)
-
-// HOW CAN BOTH BE THE SECOND ARGUMENT?????
+const selectDifferentDrivers = function (drivers, secondArgument) {
+  return secondArgument(drivers) 
+}
