55c5ab322702bcdb8cab6abcc42a140d56629289
ArslanInfinite <ali.arslan319@gmail.com>
2021-01-29 16:16:12 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function (drivers) {
+  firstTwoDrivers = drivers.slice(0, 2)
+  return firstTwoDrivers
+}
+
+const returnLastTwoDrivers = function (drivers) {
+  lastTwoDrivers = drivers.slice(-2)
+  return lastTwoDrivers
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function (multiplier) {
+  return function (fare) {
+    return multiplier * fare;
+  }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = function(drivers)
+
+// HOW CAN BOTH BE THE SECOND ARGUMENT?????
