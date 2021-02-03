32f0c26cc03a857d40b9ba75869c918148698205
William Pannell <lpann5@gmail.com>
2021-02-03 00:05:34 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivas){
+    return drivas.slice(0, 2)
+}
+const returnLastTwoDrivers = function(drivas){
+    return drivas.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function(epicInt){
+    return function(fare){
+        return fare * epicInt
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function(driverArray, eitherDriver){
+    return eitherDriver(driverArray)
+}
