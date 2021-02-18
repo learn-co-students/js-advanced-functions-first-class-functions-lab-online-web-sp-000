91d858728fb9a2c42dcf815ea1e5e912894f11f8
john million <jpmillion@comcast.net>
2021-02-18 13:39:59 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+    const answer = function() {
+        return drivers.slice(0, 2);
+    }
+    return answer();
+}
+
+function returnLastTwoDrivers(drivers) {
+    const answer = function() {
+        return drivers.slice(-2);
+    }
+    return answer();
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(num) {
+    const answer = function() {
+        return num**2;
+    }
+    return answer;
+}
+
+function fareDoubler(num) {
+    const answer = createFareMultiplier(num);
+    return (answer() / num) * 2;
+}
+
+function fareTripler(num) {
+    const answer = createFareMultiplier(num);
+    return (answer() / num) * 3;
+}
+
+function selectDifferentDrivers(drivers, fn) {
+    return fn(drivers);
+}
